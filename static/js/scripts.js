44f215a366fd7a8ceb0dde80495ad7503a1402c0
honkitng <55844706+honkitng@users.noreply.github.com>
2021-06-30 14:54:20 -0400

    Optimized scrolling to feel smoother

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,4 +1,10 @@
 var jpeg_array = new Array;
+var micrograph;
+var scroll_run;
+
+function getVar(value) {
+    return value
+};
 
 function select(value) {
     var value_index = jpeg_array.indexOf(value);
@@ -30,8 +36,6 @@ function submit() {
         alert(full_text);
     })
 };
-
-var micrograph;
 
 function increaseSize() {
     if (default_width < 1000) {
@@ -87,10 +91,6 @@ function decreaseSize() {
     };
 };
 
-function getVar(value) {
-    return value
-};
-
 function onStart() {
     var node = document.createElement('img');
     node.classList.add('rounded');
@@ -133,8 +133,6 @@ function onWindowScroll() {
     });
 };
 
-var scroll_run;
-
 function scrollDelay() {
     if (typeof scroll_run !== "undefined"){
         clearTimeout(scroll_run);
@@ -142,6 +140,78 @@ function scrollDelay() {
     scroll_run = setTimeout(function(){ onWindowScroll() }, 100);
 };
 
-window.addEventListener('scroll', scrollDelay, false);
-window.addEventListener('resize', scrollDelay, false);
-document.addEventListener('DOMContentLoaded', onStart, false);
+var image_count = 0;
+var showing = [];
+
+function onStartTest() {
+    var node = document.createElement('img');
+    node.classList.add('rounded');
+    node.name = 'micrograph';
+    node.style.width = default_width.toString().concat('px');
+    node.style.height= default_height.toString().concat('px');
+    node.src = 'jpeg/'.concat($('.image-button')[0].id);
+    $('.image-button')[0].appendChild(node);
+
+    var width = $('.image-button')[0].offsetWidth;
+    var height = $('.image-button')[0].offsetHeight;
+
+    $('.image-button').each(function(index) {
+        this.style.width = width.toString().concat('px');
+        this.style.height = height.toString().concat('px');
+
+        var max_top = window.scrollY - window.innerHeight;
+        var max_bot = window.scrollY + window.innerHeight;
+        if (max_top < this.offsetTop && this.offsetTop < max_bot) {
+            image_count += 1;
+        };
+    });
+    onWindowScrollTest();
+};
+
+function onWindowScrollTest() {
+    var new_img = []
+    var add_img = []
+
+    first_index = parseInt(window.scrollY/document.body.clientHeight * $('.image-button').length - image_count)
+    if (first_index < 0) {
+        first_index = 0;
+    };
+
+    for (i = first_index; i < first_index + image_count * 3; i++) {
+        if (i < $('.image-button').length) {
+            new_img.push(i);
+            if ($('.image-button')[i].children.length == 0) {
+                var node = document.createElement('img');
+                node.classList.add('rounded');
+                node.name = 'micrograph';
+                node.style.width = default_width.toString().concat('px');
+                node.style.height= default_height.toString().concat('px');
+                node.src = 'jpeg/'.concat($('.image-button')[i].id);
+                $('.image-button')[i].appendChild(node);
+            };
+        };
+    };
+
+    for (i = 0; i < showing.length; i++) {
+        if (!new_img.includes(showing[i])) {
+            $('.image-button')[showing[i]].children[0].remove()
+        };
+    };
+
+    showing = new_img
+};
+
+function scrollDelayTest() {
+    if (typeof scroll_run !== "undefined"){
+        clearTimeout(scroll_run);
+    };
+    scroll_run = setTimeout(function(){ onWindowScrollTest() }, 50);
+};
+
+//window.addEventListener('scroll', scrollDelay, false);
+//window.addEventListener('resize', scrollDelay, false);
+//document.addEventListener('DOMContentLoaded', onStart, false);
+
+window.addEventListener('scroll', scrollDelayTest, false);
+window.addEventListener('resize', scrollDelayTest, false);
+document.addEventListener('DOMContentLoaded', onStartTest, false);
